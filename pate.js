03125d6ef7309c605482398f25582496a7551e71
Scania113h <ligiabeatrizjahnke@gmail.com>
2024-06-07 16:44:28 -0300

    caminhao classe

--- pate.js
+++ pate.js
@@ -1,1 +1,9 @@
 console.log("aula de git")
+class Caminhao{
+    constructor(tipo,capacidade){
+        this.tipo = tipo;
+        this.capacidade = capacidade
+    }
+}
+
+const gol = new Caminhao("caminhao",5);
